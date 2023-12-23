78bdae3059c8400a143266fe3e88ee5760858a52
projectman14 <lakshyajain.14022@gmail.com>
2023-12-23 20:16:42 +0530

    More Updated

--- src/SendMainComponent.jsx
+++ src/SendMainComponent.jsx
@@ -5,15 +5,17 @@ import Designs from './assets/Designs.svg';
 
 export default function SendMailComponent() {
     return (
-        <div className='SendMailComponent'>
-            <img className='SendBtn' src={SendBtn}></img>
-            <p className='SubscribeHeadLine'>Subscribe to get information, latest news and other interesting offers about Cobham</p>
-            <div className='EmailInput'>
-                <TextField id="outlined-basic" label="Email" variant="outlined" />
-                <button className='SubscribeBtn'>Subscribe</button>
-            </div>
-            <div className='DesignDiv'>
-                <img src={Designs} className='DesignImgs' ></img>
+        <div className='SendMailMainComponent'>
+            <div className='SendMailComponent'>
+                <img className='SendBtn' src={SendBtn}></img>
+                <p className='SubscribeHeadLine'>Subscribe to get information, latest news and other interesting offers about Cobham</p>
+                <div className='EmailInput'>
+                    <TextField id="outlined-basic" label="Email" variant="outlined" />
+                    <button className='SubscribeBtn'>Subscribe</button>
+                </div>
+                <div className='DesignDiv'>
+                    <img src={Designs} className='DesignImgs' ></img>
+                </div>
             </div>
         </div>
     );
